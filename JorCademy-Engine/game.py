a972d7ed1b7784a999938c0dd181b58ca97f4a0c
Nick Jordan <nick@jorcademy.nl>
2023-07-28 14:55:31 +0200

    [FEATURE] Implemented Game Over screen.

--- JorCademy-Engine/game.py
+++ JorCademy-Engine/game.py
@@ -5,8 +5,14 @@ from level import Level
 # Levels
 level = Level("1_1")
 
-
 # TODO: Optimize performance. Performance seems to drop linearly when getting loot from lootboxes.
+
+
+# Show game over screen
+def show_game_over_screen() -> None:
+    backdrop((0, 0, 0))
+    text("GAME OVER", 50, (255, 255, 255), screen_width / 2, screen_height / 2 - 30, "fonts/pixel.ttf")
+    text(f"SCORE: {level.link.score}", 20, (255, 255, 255), screen_width / 2, screen_height / 2 + 20, "fonts/pixel.ttf")
 
 
 def setup() -> None:
@@ -17,6 +23,10 @@ def setup() -> None:
 
 
 def update() -> None:
+    if level.link.lives == 0:
+        show_game_over_screen()
+        return
+
     # Draw sky backdrop
     backdrop(level.backdrop_color)
 
